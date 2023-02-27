5b6101fac3c6b3e84f54a674ed1212ba089df8f4
ukssss <1872003115su@gmail.com>
2023-02-27 09:32:56 +0900

    nwitter |  Nweeting

--- 2. nwitter/src/routes/Home.js
+++ 2. nwitter/src/routes/Home.js
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { dbService } from 'fbase';
+import { collection, addDoc, onSnapshot, query, orderBy, serverTimestamp } from 'firebase/firestore';
+import Nweet from 'components/Nweet';
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(dbService, 'nweets'), orderBy('createdAt', 'desc'));
+    onSnapshot(q, (snapshot) => {
+      const nweetArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArr);
+    });
+  }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await addDoc(collection(dbService, 'nweets'), {
+      text: nweet,
+      createdAt: serverTimestamp(),
+      creatorId: userObj.uid,
+    });
+
+    setNweet('');
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChange} type='text' placeholder="what's on your mind?" maxLength={120}></input>
+        <input type='submit' value='Nweet' />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
